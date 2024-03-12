e4b0d2ee6d59e829b08d510c89368397a16a8b03
Am1d1v <dimadiman1adima@gmail.com>
2024-03-12 14:15:52 +0300

    Check if valid number

--- app.js
+++ app.js
@@ -12,7 +12,33 @@ function getRandomNumber(){
     // Capture user speak
 function onSpeak(e){
     const msg = e.results[0][0].transcript;
+
+    // Write out what we speak
+    writeMessage(msg);
+
+    // Check that input data is a number
+    checkNumber(msg)
 }    
+
+    // Check message againts number
+function checkNumber(msg){
+    // Convert to number
+    const num = +msg;
+
+    // Check if valid number
+    if(Number.isNaN(num)){
+        msgEl.innerHTML = '<div>That is not a valid number</div>'
+    }
+}    
+
+    // Write what user speaks
+function writeMessage(msg){
+    msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+    `
+};
+
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
